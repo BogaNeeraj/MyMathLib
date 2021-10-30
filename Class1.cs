ab81feac4b20f0fcb1a41a04e560c32fc35c6af3
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-30 22:07:16 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyMathLib
+{
+    public static class Class1
+    {
+        public static bool StartWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
